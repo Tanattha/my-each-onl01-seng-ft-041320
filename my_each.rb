0da15805c108b8f1b958bb7319fc35b0779a6b81
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-15 23:02:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,22 @@
 def my_each(array)
+  
   while array.lenght != 0
   
   array do |collection|
     puts array
   # code here
+end
+
+def my_each(array)
+  if block_given? #return true
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+   
 end
